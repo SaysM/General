85f0171ffe1524a6d8f951339754d61448b3a374
SaysM <oliveirafaelmonreo@gmail.com>
2022-06-28 00:54:12 -0300

    Estudando  JavaScript

---
+++ JavaScript/Curso_em_video/002/04.js
@@ -0,0 +1,8 @@
+
+
+// Para o node operadorees são como em na maioria das outras linguagens mas existe um operador de "identidade"
+
+//var operador = 5
+//if (operador === 5){
+//    operador = 10
+//    operador}
